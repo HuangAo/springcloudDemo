9ea6490d5da43a2b361466ad8fb2e39bafeb2dc2
huangao <huangao23@163.com>
2020-10-17 16:24:00 +0800

    消费者 订单模块实现

---
+++ cloud-consumer-order80/src/main/java/com/huang/cloud/OrderMain80.java
@@ -0,0 +1,14 @@
+package com.huang.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author :huangao
+ */
+@SpringBootApplication
+public class OrderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class,args);
+    }
+}
