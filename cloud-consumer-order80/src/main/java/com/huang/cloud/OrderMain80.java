3a64cab10fc8e138df312cc531172acdaf9eab15
huangao <huangao23@163.com>
2020-10-17 17:29:10 +0800

    将订单和支付模块注册进注册中心

--- cloud-consumer-order80/src/main/java/com/huang/cloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/huang/cloud/OrderMain80.java
@@ -2,11 +2,13 @@ package com.huang.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author :huangao
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class,args);
--- cloud-provider-payment8001/src/main/java/com/huang/cloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/huang/cloud/PaymentMain8001.java
@@ -2,11 +2,13 @@ package com.huang.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author :huangao
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
 
     public static void main(String[] args) {
