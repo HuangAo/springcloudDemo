0120087de8283fdf8fff76c441c95d1582bf3f66
huangao <huangao23@163.com>
2020-10-17 17:00:10 +0800

    创建注册中心：Eureka

---
+++ cloud-eureka-server7001/src/main/java/com/huang/cloud/EurekaServerMain.java
@@ -0,0 +1,16 @@
+package com.huang.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author :huangao
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerMain {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerMain.class,args);
+    }
+}
