ad5fb9f116a5c3c0358c1c81d5d32f2e92b68fde
huangao <huangao23@163.com>
2020-10-18 13:04:22 +0800

    Ribbon 配置自己的负载均衡策略替换默认的

---
+++ cloud-consumer-order80/src/main/java/com/huang/myrule/MySelfRule.java
@@ -0,0 +1,20 @@
+package com.huang.myrule;
+
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author :huangao
+ */
+@Configuration
+public class MySelfRule {
+
+    @Bean
+    public IRule myRule()
+    {
+        return new RandomRule();//定义为随机
+    }
+}
