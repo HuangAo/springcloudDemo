c51c20092162c66fcd76a0aac4d331d524438522
huangao <huangao23@163.com>
2020-10-19 20:52:49 +0800

    将payment集群（9001，9002）注册进Nacos

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/huang/cloud/alibaba/PaymentMain9001.java
@@ -0,0 +1,16 @@
+package com.huang.cloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author :huangao
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class,args);
+    }
+}
