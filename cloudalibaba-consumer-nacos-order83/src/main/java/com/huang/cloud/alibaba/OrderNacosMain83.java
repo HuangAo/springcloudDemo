1696349d8398a38fdb0a9e1fbc82890d55866126
huangao <huangao23@163.com>
2020-10-19 21:07:59 +0800

    服务调用者注册进Nacos

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/huang/cloud/alibaba/OrderNacosMain83.java
@@ -0,0 +1,16 @@
+package com.huang.cloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author :huangao
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderNacosMain83 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderNacosMain83.class,args);
+    }
+}
