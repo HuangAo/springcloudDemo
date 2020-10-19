eebe5255de558ebce3a3fc4ceff7e3630197524a
huangao <huangao23@163.com>
2020-10-19 13:49:06 +0800

    引入网关Gateway 配置服务提供者payment8001路由

---
+++ cloud-gateway-gateway9527/src/main/java/com/huang/cloud/GateWayMain9527.java
@@ -0,0 +1,16 @@
+package com.huang.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author :huangao
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class,args);
+    }
+}
